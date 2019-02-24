5c4f3bc2b214b064c8a75e2ede215efe7cb1c340
Eva Kato <evakato14@gmail.com>
2019-02-24 15:15:16 -0500

    photo uploader added

--- app/static/main.js
+++ app/static/main.js
@@ -1,15 +1,39 @@
+function photo() {
+    document.getElementById("typeIn").style.visibility = "hidden";
+    document.getElementById("typeIn").style.display = "none";
+    document.getElementById("toPhoto").style.visibility = "hidden";
+    document.getElementById("toPhoto").style.display = "none";
+    document.getElementById("toText").style.visibility = "visible";
+    document.getElementById("toText").style.display = "block";
+    document.getElementById("imageUpload").style.visibility = "visible";
+    document.getElementById("imageUpload").style.display = "block";
+}
+
+function text() {
+    document.getElementById("imageUpload").style.visibility = "hidden";
+    document.getElementById("imageUpload").style.display = "none";
+    document.getElementById("toText").style.visibility = "hidden";
+    document.getElementById("toText").style.display = "none";
+    document.getElementById("toPhoto").style.visibility = "visible";
+    document.getElementById("toPhoto").style.display = "block";
+    document.getElementById("typeIn").style.visibility = "visible";
+    document.getElementById("typeIn").style.display = "block";
+}
+
 $(document).ready(function() {
     $('form').on('submit', function(event) {
     	$.ajax({
 			data : {
                 city : document.getElementById("city").value,
                 county : document.getElementById("county").value,
+                state : document.getElementById("state").value,
+                material : document.getElementById("material").value
 			},
 			type : 'POST',
 			url : '/process'
 		})
 		.done(function(data) {
-			//enter output here
+			$('#recyclingInfo').text(data).show();
 		});
 		event.preventDefault();
 	})
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import string
 from pymongo import MongoClient
+import re
 app = Flask(__name__)
 
 client = MongoClient("mongodb://blueprint:aaaabbbb@blueprint-shard-00-00-jwwbc.mongodb.net:27017,blueprint-shard-00-01-jwwbc.mongodb.net:27017,blueprint-shard-00-02-jwwbc.mongodb.net:27017/test?ssl=true&replicaSet=Blueprint-shard-0&authSource=admin&retryWrites=true")
@@ -19,8 +20,9 @@ def retrieve():
     invalidChars = set(string.punctuation)
     if any(char in invalidChars for char in word):
         return;
-    result=db.command("db.USA.find( { \"state\":{ $regex:/^"+state+"/}, \"city\":{ $regex:/^"+city+"/} } )")
-    print(result)
+    result=db.USA.find( { "state": re.compile("^"+state), "city":re.compile("^"+city) } )
+    return jsonify(result)
+
 
 @app.route("/put", methods=['POST'])
 def put():
@@ -32,3 +34,34 @@ def put():
     if any(char in invalidChars for char in word):
         return;
     result=db.command("db.USA.insert( { \"state\":\""+state+"\", \"city\":\""+city+"\" } )")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    folder_name = request.form['superhero']
+    '''
+    # this is to verify that folder to upload to exists.
+    if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
+        print("folder exist")
+    '''
+    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    print(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        # This is to verify files are supported
+        ext = os.path.splitext(filename)[1]
+        if (ext == ".jpg") or (ext == ".png"):
+            print("File supported moving on...")
+        else:
+            render_template("Error.html", message="Files uploaded are not supported...")
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("complete.html", image_name=filename)
